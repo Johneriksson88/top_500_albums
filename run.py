4129befa4d665739fe02e1abbc58c17d8fd255f7
Johneriksson88 <john.e.eriksson@gmail.com>
2022-08-21 18:19:24 +0000

    Create validate_menu_choice function

--- run.py
+++ run.py
@@ -19,10 +19,18 @@ albumlist = worksheet.get_all_values()
 print("*" * 70 + "\n\n" + "Welcome to a program for analysis of The Rolling Stones top 500 albums list.\nThe list was published in 2003 with a slight update 2012.", \
     "\nIt is based on weighted votes from selected musicians, critics, and industry figures, and compiled into a list by the music magazine 'The Rolling Stone'.\n\n" + "*" * 70 + "\n")
 
-def start_menu():
-    choice_1 = int(input("Type in 1 to see the whole list, 2 to see analysis options: "))
+def validate_menu_choice(menu_choice_1):
     try:
-        if choice_1 == 1:
-            print(tabulate(albumlist, headers=["Number","Year", "Album", "Artist", "Genre"]))
-        elif choice_1 == 2:
-            print("Analysis options")
+        if int(menu_choice) != 1 or int(menu_choice) != 2:
+            raise ValueError(
+                f"Choice can only be 1 or 2, you provided {menu_choice}"
+            )
+        except ValueError as e:
+            print(f"Invalid data: {e}, please try again\n")
+            return False
+        return True
+            
+
+menu_choice_1 = int(input("Type in 1 to see the whole list, 2 to see analysis options: "))
+print(tabulate(albumlist, headers=["Number","Year", "Album", "Artist", "Genre"]))
+print("Analysis options")
