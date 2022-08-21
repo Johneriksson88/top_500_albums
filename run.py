ebe28e3ade6608905f588851b944dd4e7465dec5
Johneriksson88 <john.e.eriksson@gmail.com>
2022-08-21 18:02:28 +0000

    Add function for start menu

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import gspread
 from tabulate import tabulate
-import pandas as pd
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
@@ -14,11 +13,16 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('albumlist')
 
-albumlist = SHEET.worksheet('albumlist')
+worksheet = SHEET.worksheet('albumlist')
+albumlist = worksheet.get_all_values()
 
 print("*" * 70 + "\n\n" + "Welcome to a program for analysis of The Rolling Stones top 500 albums list.\nThe list was published in 2003 with a slight update 2012.", \
     "\nIt is based on weighted votes from selected musicians, critics, and industry figures, and compiled into a list by the music magazine 'The Rolling Stone'.\n\n" + "*" * 70 + "\n")
 
-choice_1 = input("Type in 1 to see the whole list, 2 to see analysis options: ")
-print(f"Choice was {choice_1}")
-
+def start_menu():
+    choice_1 = int(input("Type in 1 to see the whole list, 2 to see analysis options: "))
+    try:
+        if choice_1 == 1:
+            print(tabulate(albumlist, headers=["Number","Year", "Album", "Artist", "Genre"]))
+        elif choice_1 == 2:
+            print("Analysis options")
