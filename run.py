73da204adb00917f4e83954419b61fad81424bc5
Johneriksson88 <john.e.eriksson@gmail.com>
2022-08-24 21:44:57 +0000

    Set basic layout for analysis options menu

--- run.py
+++ run.py
@@ -24,13 +24,13 @@ print("*" * 70 + "\n\n" + "Welcome to a program for analysis of The Rolling Ston
 
 def start_menu():
     while True:
-        menu_choice_1 = input(
+        menu_choice = input(
             "Type in 1 to see the whole list, 2 to see analysis options: ")
-        if menu_choice_1 == "1":
+        if menu_choice == "1":
             print(tabulate(albumlist, headers=[
                   "Number", "Year", "Album", "Artist", "Genre"]))
             break
-        elif menu_choice_1 == "2":
+        elif menu_choice == "2":
             analysis_options()
             break
         print("Input must be 1 or 2, try again.")
@@ -40,7 +40,17 @@ def analysis_options():
     while True:
         print("\nMake one of the following choices:\n1. Search for artist\n2. Get top 10 list of a given parameter\n3.",
               "Get most occuring genre per decade\n4. Add your own album(s) to the list")
-        search_artist()
+        menu_choice = input()
+        if menu_choice == "1":
+            search_artist()
+        elif menu_choice == "2":
+            get_top_10()
+        elif menu_choice == "3":
+            print("Enter a decade (e.g. 1970, 1990 etc.)")
+            #most_occurring_per_decade()
+        elif menu_choice == "4":
+            print("Add album: ")
+            #add_album()
         break
 
 
@@ -71,13 +81,13 @@ def get_top_10():
     """
 
     print("\nChoose a variable to see a top 10 list of:\n1. Artist\n2. Year\n3. Decade\n4. Genre")
-    menu_choice_2 = input()
+    menu_choice = input()
 
     
     # Counts the number of values in the 4th(artist) column and prints the 10 most common artists in a table
     # and a line presenting the most popular artist and the percentage of their placements
     
-    if menu_choice_2 == "1":
+    if menu_choice == "1":
         artist_count = Counter(worksheet.col_values(4)).most_common(10)
         top_1_artist = artist_count[0]
         print(tabulate(artist_count, headers=["Artist", "No. of placements"]))
@@ -88,7 +98,7 @@ def get_top_10():
     # Counts the number of values in the 2nd(year) column and prints the 10 most common years in a table
     # and a line presenting the most popular year and the percentage of its placements
 
-    elif menu_choice_2 == "2":
+    elif menu_choice == "2":
         year_count = Counter(worksheet.col_values(2)).most_common(10)
         top_1_year = year_count[0]
         top_1_year_int = top_1_year[1]
@@ -101,7 +111,7 @@ def get_top_10():
     # and prints a top list of the decades represented
     
 
-    elif menu_choice_2 == "3":
+    elif menu_choice == "3":
         years = worksheet.col_values(2)
         ints = [eval(i) for i in years]
         decades = []
@@ -117,7 +127,7 @@ def get_top_10():
     # and a line presenting the most popular genre and the percentage of its placements
     
 
-    elif menu_choice_2 == "4":
+    elif menu_choice == "4":
         genre_count = Counter(worksheet.col_values(5)).most_common(10)
         top_1_genre = genre_count[0]
         top_1_genre_int = top_1_genre[1]
